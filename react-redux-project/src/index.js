4111f99fd7428dc0569bb3a554fb9b55b5f75075
YasirHasn9 <hassanyasir223@gmail.com>
2020-04-09 02:49:07 -0700

    react

---
+++ react-redux-project/src/index.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+
+/*
+the benefit of react component
+easy to understand 
+less code which miss hight performance 
+avid using the this keyword
+easy to test
+performance 
+
+useMemos avoid needless re render for the performance
+
+the only thing that component function can do is the 
+componentDidError()
+and 
+getSnapShotBeforeUpdate()
+  */
